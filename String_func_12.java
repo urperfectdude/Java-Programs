875c3046229f2968f1376f9340a0e2ed835e22d1
Prashant Nayak <41485907+urperfectdude@users.noreply.github.com>
2022-05-28 16:58:38 +0530

    Update String_func_12.java

--- String_func_12.java
+++ String_func_12.java
@@ -4,7 +4,7 @@ public class String_func_12 {
 
 	public static void main(String[] args) {
 		
-		String name = "Jai Gora";
+		String name = "Prashant Nayak";
 		System.out.println(name.toUpperCase());
 		
 	}

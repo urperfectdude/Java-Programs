542b5a8fb837f01e47b39bcbd06927dea644c699
Prashant Nayak <41485907+urperfectdude@users.noreply.github.com>
2022-05-28 16:59:36 +0530

    Update String_func_14.java

--- String_func_14.java
+++ String_func_14.java
@@ -4,7 +4,7 @@ public class String_func_14 {
 
 	public static void main(String[] args) {
 		
-		String name = "    Jai Gora    ";
+		String name = "    Prashant Nayak   ";
 		System.out.println(name.trim());
 		
 	}
